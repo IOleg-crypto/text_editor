df456c2edee793b4a9a1d79600702d996d79a1d9
IOleg-sus <guchkooleg7@gmail.com>
2024-04-07 10:32:52 +0300

    changes

--- cpp_library.h
+++
@@ -1,7 +0,0 @@
-#ifndef CPP_LIBRARY_H
-#define CPP_LIBRARY_H
-
-#include <iostream>
-#include <string>
-
-#endif // CPP_LIBRARY_H
--- main_file.cpp
+++
@@ -1,7 +0,0 @@
-#include "cpp_library.h"
-
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
