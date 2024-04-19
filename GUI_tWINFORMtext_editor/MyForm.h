b77ae40205c6e8489da7782f5165e58266f6f165
IOleg-sus <guchkooleg7@gmail.com>
2024-04-19 18:41:37 +0300

    added checkbox and richtextbox

--- GUI_tWINFORMtext_editor/MyForm.h
+++ GUI_tWINFORMtext_editor/MyForm.h
@@ -34,6 +34,9 @@ namespace GUItWINFORMtexteditor {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,18 +51,58 @@ namespace GUItWINFORMtexteditor {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
 			this->SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(31, 39);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(1123, 465);
+			this->richTextBox1->TabIndex = 0;
+			this->richTextBox1->Text = L"";
+			//this->richTextBox1->BackColor = System::Drawing::Color::Red; #DEBUG : Tested
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(31, 520);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(95, 20);
+			this->checkBox1->TabIndex = 1;
+			this->checkBox1->Text = L"Check color of label field to black";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			this->checkBox1->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox1_CheckedChanged);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1166, 552);
-			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->richTextBox1);
+			this->Name = L"Notepad";
+			this->Text = L"Notepad";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void checkBox1_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+		if (checkBox1->Checked)
+		{
+			this->richTextBox1->BackColor = System::Drawing::Color::Black;
+			this->richTextBox1->ForeColor = System::Drawing::Color::White;
+		}
+		else
+		{
+			this->richTextBox1->BackColor = System::Drawing::Color::White;
+		}
+	}
+		  
 	};
 }
