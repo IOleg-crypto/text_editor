750437055c64aea6630a9d6cf79150a6a5ab6712
IOleg-sus <guchkooleg7@gmail.com>
2024-04-19 18:08:11 +0300

    added files : CRL text editor

---
+++ GUI_tWINFORMtext_editor/MyForm.cpp
@@ -0,0 +1,16 @@
+﻿#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) 
+{
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles;
+	GUItWINFORMtexteditor::MyForm form;
+	Application::Run(% form);
+
+}
+
+
