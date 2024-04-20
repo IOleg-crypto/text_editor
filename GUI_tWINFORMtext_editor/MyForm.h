60a4b9f8dec70bd2800f4d2d5cf6c6794c87477a
IOleg-sus <guchkooleg7@gmail.com>
2024-04-20 10:09:39 +0300

    Added new features

--- GUI_tWINFORMtext_editor/MyForm.h
+++ GUI_tWINFORMtext_editor/MyForm.h
@@ -22,7 +22,7 @@ namespace GUItWINFORMtexteditor {
 			InitializeComponent();
 			//
 			//TODO: Add the constructor code here
-			//
+			
 		}
 
 	protected:
@@ -39,6 +39,13 @@ namespace GUItWINFORMtexteditor {
 	private: System::Windows::Forms::RichTextBox^ richTextBox1;
 	private: System::Windows::Forms::CheckBox^ checkBox1;
 	private: System::Windows::Forms::Button^ button1;
+
+
+	private: System::Windows::Forms::CheckBox^ checkBox2;
+	private: System::Windows::Forms::ProgressBar^ progressBar1;
+
+
+
 	protected:
 
 	private:
@@ -57,15 +64,21 @@ namespace GUItWINFORMtexteditor {
 			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
 			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
+			this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
 			this->SuspendLayout();
 			// 
 			// richTextBox1
 			// 
+			this->richTextBox1->AutoSize = true;
 			this->richTextBox1->Location = System::Drawing::Point(20, 37);
+			this->richTextBox1->Multiline = false;
 			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->ShortcutsEnabled = false;
 			this->richTextBox1->Size = System::Drawing::Size(1134, 474);
 			this->richTextBox1->TabIndex = 0;
 			this->richTextBox1->Text = L"";
+			this->richTextBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::richTextBox1_TextChanged);
 			// 
 			// checkBox1
 			// 
@@ -80,22 +93,46 @@ namespace GUItWINFORMtexteditor {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(328, 517);
+			this->button1->AutoSize = true;
+			this->button1->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
+			this->button1->Location = System::Drawing::Point(453, 516);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(74, 26);
 			this->button1->TabIndex = 2;
 			this->button1->Text = L"Show info";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// checkBox2
+			// 
+			this->checkBox2->AutoSize = true;
+			this->checkBox2->Location = System::Drawing::Point(293, 520);
+			this->checkBox2->Name = L"checkBox2";
+			this->checkBox2->Size = System::Drawing::Size(128, 20);
+			this->checkBox2->TabIndex = 4;
+			this->checkBox2->Text = L"Enable readonly";
+			this->checkBox2->UseVisualStyleBackColor = true;
+			this->checkBox2->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox2_CheckedChanged);
+			// 
+			// progressBar1
+			// 
+			this->progressBar1->Location = System::Drawing::Point(605, 517);
+			this->progressBar1->Name = L"progressBar1";
+			this->progressBar1->Size = System::Drawing::Size(160, 29);
+			this->progressBar1->TabIndex = 5;
+			this->progressBar1->Click += gcnew System::EventHandler(this, &MyForm::progressBar1_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1166, 552);
+			this->Controls->Add(this->progressBar1);
+			this->Controls->Add(this->checkBox2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->checkBox1);
 			this->Controls->Add(this->richTextBox1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::SizableToolWindow;
 			this->Name = L"MyForm";
 			this->Text = L"Notepad";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
@@ -119,10 +156,28 @@ namespace GUItWINFORMtexteditor {
 			this->richTextBox1->ForeColor = System::Drawing::Color::Black; //#Fixed bug with text;
 		}
 	}
-		  
+	//Showing short infomation	  
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		MessageBoxW(NULL, L"", L"", MB_ICONINFORMATION); //#FIX ME : error extern ....
+		MessageBox::Show("Notepad had been made by I#Oleg \n @Copyright 2024" , "\tInformation" , MessageBoxButtons::OK , MessageBoxIcon::Information);
 			
 	}
+private: System::Void richTextBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	this->richTextBox1->Font = (gcnew System::Drawing::Font(L"Tahoma", 20.0F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+		static_cast<System::Byte>(0)));
+}
+/* Bullshut
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	bool buttonwasclicked = false;
+	if (buttonwasclicked == true) { this->button2->Text = L"Disable readonly"; this->richTextBox1->ReadOnly = true; }
+	else  this->button2->Text = L"Enable readonly";  this->richTextBox1->ReadOnly = false;
+}
+*/
+	private: System::Void checkBox2_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	if (checkBox2->Checked) {this->checkBox2->Text = L"Disable readonly"; this->richTextBox1->ReadOnly = true;
+	}
+	else { this->checkBox2->Text = L"Enable readonly";  this->richTextBox1->ReadOnly = false; }
+}
+private: System::Void progressBar1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
